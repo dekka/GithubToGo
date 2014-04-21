baa37b9f89937b949d5fb83e7d60a9e361427569
Reed Sweeney <reed.sweeney@gmail.com>
2014-04-21 09:22:21 -0700

    Initial Commit

---
+++ GithubToGo/RSAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  RSAppDelegate.h
+//  GithubToGo
+//
+//  Created by Reed Sweeney on 4/21/14.
+//  Copyright (c) 2014 Reed Sweeney. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RSAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ GithubToGo/RSViewController.m
@@ -0,0 +1,29 @@
+//
+//  RSViewController.m
+//  GithubToGo
+//
+//  Created by Reed Sweeney on 4/21/14.
+//  Copyright (c) 2014 Reed Sweeney. All rights reserved.
+//
+
+#import "RSViewController.h"
+
+@interface RSViewController ()
+
+@end
+
+@implementation RSViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
